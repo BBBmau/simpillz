2d2dc9d67c497849c20e45e0ec39a459b6919570
Mark Gacoka Mbui <markgacoka@gmail.com>
2022-10-16 01:39:26 -0700

    Add dashboard design - card widget, name, dosage and scrollable container

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -11,10 +12,21 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.white,
+          size: 35,
+        ),
+        shadowColor: Colors.transparent,
+        leadingWidth: 100,
+        toolbarHeight: 100,
+      ),
       body: SafeArea(
         child: Center(
           child: Stack(
             children: [
+              // background gradient
               Container(
                 decoration: const BoxDecoration(
                   gradient: LinearGradient(
@@ -27,14 +39,83 @@ class _DashboardPageState extends State<DashboardPage> {
                   ),
                 ),
               ),
-              const Center(
-                child: SizedBox(
-                  height: 115,
-                  width: 115,
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/old_man.jpeg"),
+              // Profile image column
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 150,
+                    child: Center(
+                      child: SizedBox(
+                        height: 115,
+                        width: 115,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage("assets/images/old_man.jpeg"),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  const Text(
+                    "John Doe",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    "3 left",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                  Expanded(
+                    child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(40.0),
+                            topLeft: Radius.circular(40.0),
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 40),
+                              child: TextButton(
+                                style: TextButton.styleFrom(
+                                  padding: const EdgeInsets.all(20),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15)),
+                                  backgroundColor: Colors.grey,
+                                ),
+                                onPressed: (() => {}),
+                                child: Row(
+                                  children: const [
+                                    SizedBox(width: 20),
+                                    Expanded(
+                                        child: Text(
+                                      "Levothyroxine",
+                                      style: TextStyle(color: Colors.white),
+                                    )),
+                                    Icon(
+                                      Icons.more_vert,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                ],
               ),
             ],
           ),
